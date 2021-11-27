0cf6960c9527dc13c90c8d11fad6bc682bec5783
mryhryki <mryhryki@gmail.com>
2021-11-27 19:29:51 +0900

    Add example routing

--- index.ts
+++ index.ts
@@ -1,5 +1,28 @@
 import { serve } from "https://deno.land/std@0.116.0/http/server.ts";
 
-serve(() => new Response("Hello World\n"));
+const PORT = 8080;
 
-console.log("http://localhost:8000/");
+serve((request): Response => {
+  const url = new URL(request.url);
+  const path = url.pathname;
+
+  if (path === "/") {
+    return new Response("<h1>example-deno-http-server</h1>", {
+      headers: {
+        "Content-Type": "text/html; charset=utf8",
+      },
+    });
+  } else if (path.startsWith("/test/")) {
+    return new Response(
+      JSON.stringify({ test: true, param: path.substring(6) }, null, 2),
+      {
+        headers: {
+          "Content-Type": "application/json; charset=utf8",
+        },
+      },
+    );
+  }
+  return new Response("Not Found", { status: 404 });
+}, { addr: `:${PORT}` });
+
+console.log(`http://localhost:${PORT}/`);
