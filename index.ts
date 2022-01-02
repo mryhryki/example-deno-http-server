871b8fb0a9b1dbf4462486a1780dd355e0355aec
mryhryki <mryhryki@gmail.com>
2022-01-02 23:15:39 +0900

    Use serve() method
    https://deno.com/deploy/docs/hello-world

--- index.ts
+++ index.ts
@@ -1,7 +1,6 @@
-// HTTP Server を起動
+import { serve } from "https://deno.land/std@0.114.0/http/server.ts";
+
 const PORT = 8080;
-const server = Deno.listen({ port: PORT });
-console.log(`Port: ${PORT}`);
 
 /**
  * HTTPリクエストのハンドラ
@@ -12,17 +11,17 @@ const handleHttpRequest = async (request: Request): Promise<Response> => {
   console.log("Request:", request.method, request.url);
   const { pathname, search } = new URL(request.url);
 
-  if (request.method === "POST") {
-    // POST リクエストの場合は、リクエストボディを返す
-    const payload = await request.text();
-    const data = JSON.stringify({ pathname, payload }, null, 2);
+  if (request.method === "GET") {
+    // GET リクエストの場合は、クエリパラメータを返す
+    const data = JSON.stringify({ pathname, search }, null, 2);
     console.log("Request Data:", data);
     return new Response(data, {
       headers: { "Content-Type": "application/json" },
     });
-  } else if (request.method === "GET") {
-    // GET リクエストの場合は、クエリパラメータを返す
-    const data = JSON.stringify({ pathname, search }, null, 2);
+  } else if (request.method === "POST") {
+    // POST リクエストの場合は、リクエストボディを返す
+    const payload = await request.text();
+    const data = JSON.stringify({ pathname, payload }, null, 2);
     console.log("Request Data:", data);
     return new Response(data, {
       headers: { "Content-Type": "application/json" },
@@ -31,14 +30,5 @@ const handleHttpRequest = async (request: Request): Promise<Response> => {
   return new Response("Not Found", { status: 404 });
 };
 
-// リクエストを待機
-for await (const conn of server) {
-  (async () => {
-    const httpConn = Deno.serveHttp(conn);
-    for await (const requestEvent of httpConn) {
-      await requestEvent.respondWith(
-        await handleHttpRequest(requestEvent.request),
-      );
-    }
-  })();
-}
+console.log(`Listening on http://localhost:${PORT}/`);
+await serve(handleHttpRequest, { addr: `:${PORT}` });
