7509e67993eefb563b0c010ad37d680574792f6d
Moriya Hiroyuki <mryhryki@gmail.com>
2022-01-08 06:40:58 +0900

    Update index.ts

--- index.ts
+++ index.ts
@@ -3,12 +3,12 @@ import { serve } from "https://deno.land/std@0.114.0/http/server.ts";
 const PORT = 8080;
 
 const handler = async (request: Request): Promise<Response> => {
-  console.log("[TEST] Request:", request.method, request.url);
+  console.log("Request:", request.method, request.url);
   const { pathname, search } = new URL(request.url);
 
   if (request.method === "GET") {
     // GET リクエストの場合は、クエリパラメータを返す
-    const data = JSON.stringify({ pathname, search }, null, 2);
+    const data = JSON.stringify({ pathname, search, test: true }, null, 2);
     console.log("Request Data:", data);
     return new Response(data, {
       headers: { "Content-Type": "application/json" },
@@ -16,13 +16,13 @@ const handler = async (request: Request): Promise<Response> => {
   } else if (request.method === "POST") {
     // POST リクエストの場合は、リクエストボディを返す
     const payload = await request.text();
-    const data = JSON.stringify({ pathname, payload }, null, 2);
+    const data = JSON.stringify({ pathname, payload, test: true }, null, 2);
     console.log("Request Data:", data);
     return new Response(data, {
       headers: { "Content-Type": "application/json" },
     });
   }
-  return new Response("Not Found", { status: 404 });
+  return new Response("[TEST] Not Found", { status: 404 });
 };
 
 console.log(`Listening on http://localhost:${PORT}/`);
